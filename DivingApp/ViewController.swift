9bde10bfed88dd0c10411438fb9ccbe32f3a8877
ALi Moosa <201902206@student.polytechnic.bh>
2022-12-12 18:22:20 +0300

    Initial Commit

---
+++ DivingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DivingApp
+//
+//  Created by ALI MOOSA on 12/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
