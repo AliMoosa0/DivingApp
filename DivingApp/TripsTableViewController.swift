fb4be3fadb44350d2fa24e282d3bfbeba13737eb
safaHusain <safa.husain07@gmail.com>
2022-12-20 11:33:34 +0300

    tab bar sorted, trips table view working

--- DivingApp/TripsTableViewController.swift
+++ DivingApp/TripsTableViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class TripsTableViewController: UITableViewController {
 
-
+    var trips = [Trip]()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let savedTrips = Trip.loadTrips(){
+            trips = savedTrips
+        }else{
+            trips = Trip.loadSampleLoad()
+        }
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -29,21 +34,29 @@ class TripsTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
+ */
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return trips.count
     }
-*/
-    /*
+
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tripCell", for: indexPath)
+        
+        let trip = trips[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = trip.title
+        content.secondaryText = "\(trip.location) - \(trip.tripDate)"
+        cell.contentConfiguration = content
+        
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
