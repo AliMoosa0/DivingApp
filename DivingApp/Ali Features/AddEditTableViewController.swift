d9dfad99cdaff5458ba6c70416f2392b9e41fcab
ALi Moosa <201902206@student.polytechnic.bh>
2022-12-20 11:52:45 +0300

    adding Add/Edit view controller

--- DivingApp/Ali Features/AddEditTableViewController.swift
+++ DivingApp/Ali Features/AddEditTableViewController.swift
@@ -43,7 +43,7 @@ class AddEditTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+/*
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
@@ -53,7 +53,7 @@ class AddEditTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
-
+*/
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
