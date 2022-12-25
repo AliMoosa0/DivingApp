5dd45911287d0b7f1458cba2c506c9a953afbd35
ALi Moosa <201902206@student.polytechnic.bh>
2022-12-25 18:21:18 +0300

    home

--- DivingApp/Ali Features/HomeVC.swift
+++ DivingApp/Ali Features/HomeVC.swift
@@ -8,15 +8,61 @@
 import UIKit
 
 class HomeVC: UIViewController {
+var trips = [Trip]()
+    @IBOutlet weak var noOfTrips: UILabel!
+   
+    @IBOutlet weak var noOfDives: UILabel!
+    func numberOfTrips(trips :[Trip]){
+        
+       var tripCount = trips.count
+        
+        noOfTrips.text = "Number of trips:\n\(String(tripCount))"
+    }
+   
+    
+    
+    
+    
+    
+    func numberOfDives(trips :[Trip]){
+        var noDives : Int = 0
+        for trip in trips {
+            noDives += trip.dives!.count
+        }
+        
+        noOfDives.text = "Number of dives:\n\(String(noDives))"
+    }
+    
+    @IBOutlet weak var maxDepthLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    func maxDepthDives(trips: [[Dive]]) -> [Dive] {
+        let dives = trips.flatMap { trip in
+            trip
+        }
+        let maxDepth = dives.max { a, b in
+            a.maxDepth ?? 0.00< b.maxDepth ?? 0.00
+        }
+        return maxDepth.map { [$0] } ?? []
+    }
 
-    
-    
-    
+*/
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        trips = Trip.loadSampleLoad()
+        numberOfTrips(trips: trips)
+        numberOfDives(trips : trips)
+        
+        
         // Do any additional setup after loading the view.
     }
     
